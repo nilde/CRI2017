4e381c36f118d6be381ee2c9227e55e6a8569611
nilde <Nil.Domene@e-campus.uab.cat>
2017-10-14 19:27:24 +0200

    Modificaciones que solucionan diversos errores en la generacion de estructuras

--- read_files.py
+++ read_files.py
@@ -3,7 +3,7 @@ Esta clase genera todas las lecturas de los ficheros y genera las estructuras qu
 '''
 class StructureCreator:
 	def __init__(self,file_crossword):
-		#REVISADA
+		#REVISADA / FUNCIONA
 
 		#Leemos el crucigrama de fichero
 		self.crossword=open(file_crossword, 'r')
@@ -31,51 +31,58 @@ class StructureCreator:
 		
 
 	def OcuppiedPositionsGenerator(self):
-		#IMPORTANTE:Llamar antes a la funcion que genere las orientaciones(wordOrientation) y a la funcion que detecta los diferentes tamaños de las palabras (lookWidths)
+		# REVISADA / FUNCIONA (MIRAR QUE NO LA LIE)
+		#IMPORTANTE:Llamar antes a la funcion que genere las orientaciones(wordOrientation) y a la funcion que detecta los diferentes tamanos de las palabras (lookWidths)
 		#Estructura que usa (1)->self.orient_words_list=['1H','1V',...] 
 		#Output(1)->genera una estructura self.occupied_positions=[[0,3,0,4,0,5],[...]]] self.occupied_positions[0] contiene todas las posiciones de la palabra 1
 		#Output(2)->genera una estructura de self.cross=[[palabra_1,palabra_2,pos_x,pos_y],[palabra_1,palabra_2,pos_x,pos_y],...] self.cross[0] contiene [palabra_1,palabra_2,pos_x,pos_y]
 
 		occupied_for_word=[]
 		#Para cada palabra generamos las casillas que ocupa para poder encontrar a posteriori los diferentes cruces entre las palabras
-		for num_word,orientation in enumerate(self.orient_words_list):
+		for indice,palabra in enumerate(self.orient_words_list):
+			if len(palabra) == 2:
+				letra = palabra[1]
+				numero= int(palabra[0])
+			else:
+				letra=palabra[2]
+				numero=int(palabra[0])*10+int(palabra[1])
+
 			occupied_for_word[:]=[]
-			associated_squares=self.longitudes[num_word]
-			initial_pos=self.pos_init_words[self.orient_words_values[num_word]]
+			associated_squares=self.longitudes[indice]
+			initial_pos = list(self.pos_init_words[numero-1])
 
 			#Segundo caracter contiene la orientacion ej. (1H) si es horizontal incrementamos los contadores en el eje de las Y.
-			if orientation[1]=='H':
+			if letra =='H':
 				for offset in range(associated_squares):
-					occupied_for_word.extend([initialpos[0],initial_pos[1]+offset])
-
+					occupied_for_word.append([initial_pos[0],initial_pos[1]+offset])
 			#Sino incrementamos el contador en el eje de las X.
 			else:
 				for offset in range(associated_squares):
-					occupied_for_word.extend([initialpos[0]+offset,initial_pos[1]])
-			
+					occupied_for_word.append([initial_pos[0]+offset,initial_pos[1]])
 			#Anadimos las posiciones que ocupa cada palabra a la lista que contiene todas las ocupaciones
-			self.occupied_positions.append([occupied_for_word])
-
-	
-		skip=0
-		#Las posiciones de la matriz representan una palabra
+			self.occupied_positions.append(occupied_for_word[:])
+			
+		
 		each_cross=[]
+		#Las posiciones de la matriz representan una palabra
 		for indice_1,diff_pos_word in enumerate(self.occupied_positions):
-			each_cross[:]=[]
-			skip+=1
-			if index==len(occupied_positions)-1:
-				break
-			for word in self.occupied_positions[skip:]:
-				st1=set(tuple(diff_pos_word))
-				st2=set(tuple(word))
-				cruce=st1.intersection(st2)
-				if len(cruce) > 0:
-					palabra_1=self.orient_words_list[indice_1]
-					palabra_2=self.orient_words_list[self.occupied_positions.index(word)]
-					each_cross.extend(palabra_1,palabra_2,cruce[0],cruce[1])
-			#Anadimos en el caso de que se crucen en alguna posicion si la posicion 
-				self.cross.append(each_cross)
+			for indice_2,word in enumerate(self.occupied_positions):
+				each_cross[:]=[]
+				for i in diff_pos_word:
+					if i in word and indice_1 != indice_2:
+						print i
+						each_cross.extend(([self.orient_words_list[indice_1],self.orient_words_list[indice_2],i[0],i[1]]))
+						self.cross.append(each_cross[:])
+						each_cross[:]=[]
 
+				
+		#eliminar las permutaciones
+		for comb_1 in self.cross:
+			p1=comb_1[0]
+			p2=comb_1[1]
+			for indice_2,comb_2 in enumerate(self.cross):
+				if comb_2[0] == p2 and comb_2[1] == p1:
+					self.cross.pop(self.cross.index(comb_2))
 
 	def generate_index_cross(self):
 		#REVISADA
@@ -95,7 +102,7 @@ class StructureCreator:
 			pos_ocupadas_2=self.occupied_positions[indice_lista_ocupadas_2]
 
 			#Por ultimo alargamos la longitud de la lista auxiliar con las indices de las posiciones que ocupan 
-			auxiliar.extend((posiciones_ocupadas_1.index([each_cross[3],each_cross[4]])),posiciones_ocupadas_2.index([each_cross[3],each_cross[4]]))#Buscamos a que indice de la palabra corresponde
+			auxiliar.extend((pos_ocupadas_1.index([each_cross[3],each_cross[4]])),pos_ocupadas_2.index([each_cross[3],each_cross[4]]))#Buscamos a que indice de la palabra corresponde
 			
 			#Anadimos la estructura intermedia a la final
 			self.index_cross.append(auxiliar)
@@ -103,7 +110,7 @@ class StructureCreator:
 
 
 	def initialPosWords(self):
-		#REVISADA
+		#REVISADA / FUNCIONA
 		# Miramos la cuadricula y buscamos numeros mayores que 0.
 		#Output:self.pos_init_words[]
 		for num_row,row in enumerate(self.squares):
@@ -116,20 +123,21 @@ class StructureCreator:
 
 
 	def wordOrientation(self):
-		#REVISADA
+		#REVISADA / FUNCIONA
 		#En teoria estan por orden asi que no es necesario usar metodos como el insert ya que se revisan las cordenadas por orden.
 		#Output: self.orients_words_list es una lista que contiene una lista para cada uno de las aplabras indicando la orientacion en forma de una lista de chars H, V
 		
-		for position in self.pos_init_words:
-
+		for index,position in enumerate(self.pos_init_words):
+			index+=1
 			#Tratamiento primera fila
 			if position[0] == 0:
 				#Ultima pos primera fila
 				if position[1] == len(self.squares[0]) - 1:
 					self.orient_words_list.append(str(index)+'V')
 
+
 				#Dos corchetes o primera casilla genera 2 orientaciones si en la casilla [1][0] no hay una almohadilla
-				elif self.squares[0][position[1]-1] == '#' or (position[0] == 0 and self.squares[1][0] != '#'):
+				elif self.squares[0][position[1]-1] == '#' or (position[1] == 0 and self.squares[1][0] != '#'):
 					self.orient_words_list.append(str(index)+'H')
 					self.orient_words_list.append(str(index)+'V')
 
@@ -185,25 +193,31 @@ class StructureCreator:
 					self.orient_words_list.append(str(index)+'H')
 
 	def lookWidths(self):
-		#REVISADA
+		#REVISADA / FUNCIONA
 		#Comprobamos las longitudes de las palabras horizontales
 		#Output:lista de longitudes self.longitudes[3,4,5,...] que corresponden 1 a 1 con las posiciones de self.orient_word_list=[1H,1V,...] 
 	
 		len_max=0
 		height=len(self.squares)
 		width=len(self.squares[0])
-
 		#Para cada palabra que tenemos
-		for index,palabra in enumerate(self.orient_words_list):
-			coord=self.pos_init_words[index].copy()
+		aux=self.pos_init_words[:]
+		for palabra in self.orient_words_list:
+			if len(palabra) == 2:
+				letra = palabra[1]
+				numero= int(palabra[0])
+			else:
+				letra=palabra[2]
+				numero=int(palabra[0])*10+int(palabra[1])
+			
+			coord=aux[numero-1][:]
 			counter=0
-
 			#Orientacion horizontal
-			if palabra[1] == 'H':
+			if letra == 'H':
 				len_max=width-coord[1]
-
+				
 				#Mientras no encontremos un corchete y la medida sea valida contamos lo que ocupa la palabra
-				while (self.squares[coord[0]][coord[1]]!='#' and counter < len_max):
+				while (counter < len_max and self.squares[coord[0]][coord[1]]!='#'):
 					coord[1]+=1
 					counter+=1
 				self.longitudes.append(counter)
@@ -211,9 +225,8 @@ class StructureCreator:
 			#Orientacion vertical
 			else:
 				len_max=height-coord[0]
-
 				#Mientras no encontremos un corchete y la medida sea valida contamos lo que ocupa la palabra
-				while (self.squares[coord[0]][coord[1]] !=' #' and counter < len_max):
+				while (counter < len_max and self.squares[coord[0]][coord[1]]!= '#'):
 					coord[0]+=1
 					counter+=1
 				self.longitudes.append(counter)
@@ -236,20 +249,22 @@ class StructureCreator:
 
 
 	def __str__(self):
-		#REVISADA
+		#REVISADA / FUNCIONA
 		#Funcion que sirve para comprobar el correcto funcionamiento del sistema generador de estructuras.
 		#Output: Muestra por pantalla todas las estructuras importantes genradas
-		print "Casillas: " + self.squares
-		print "Palabras separadas (indices): " + self.sep_words_dict
-		print "Posiciones ocupadas por las palabras: " + self.occupied_positions
-		print "Posiciones iniciales ocupadas: " + self.pos_init_words
-		print "Longitudes de las palabras: " + self.longitudes
-		print "Orientacion de las palabras: " + self.orient_words_list
-		print "Palabras: " + self.orient_words_values
-		print "Cruces: "  + self.cross
-		print "Indices de cruces: " + self.index_cross
-		print "Palabra de maxima longitud: " + self.maxima_longitud
-		print "Lista de palabras que comparten longitudes: " + self.palabras_juntas
+		print "Casillas: " + str(self.squares)
+		print "Palabras separadas (indices): " + str(self.sep_words_dict)
+		print "Posiciones ocupadas por las palabras: " + str(self.occupied_positions)
+		print "Posiciones iniciales ocupadas: " + str(self.pos_init_words)
+		print "Longitudes de las palabras: " + str(self.longitudes)
+		print "Orientacion de las palabras: " + str(self.orient_words_list)
+		print "Cruces: "  + str(self.cross)
+		print "Indices de cruces: " + str(self.index_cross)
+		print "Palabra de maxima longitud: " + str(self.maxima_longitud)
+		print "Lista de palabras que comparten longitudes: " + str(self.palabras_juntas)
+		print "Lista de punteros a la estructura de las palabras compartidas: " + str(self.domain_pointers)
+		return ''
+
 
 
 
